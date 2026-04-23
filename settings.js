4eded7f038263f1d36be7c6aa2085233260b66a1
mega-aaam <agungdwiprnm@gmail.com>
2026-04-23 21:24:49 +0700

    Create settings.js

---
+++ settings.js
@@ -0,0 +1,98 @@
+const fs = require('fs');
+const chalk = require('chalk');
+
+/*
+	* Create By mega - PML
+	* Whatsapp : wa.me/6285853204842
+*/
+
+//~~~~~~~~~~~~< GLOBAL SETTINGS >~~~~~~~~~~~~\\
+
+global.owner = ["6288989149850","6285853204842"] // ['628','628'] 2 owner atau lebih
+global.author = 'MEGA - PML'
+global.botname = 'PML BOT'
+global.packname = 'Bot WhatsApp'
+global.timezone = 'Asia/Jakarta'
+global.locale = 'en'
+global.listprefix = ['+','!','.']
+
+global.listv = ['•','●','■','✿','▲','➩','➢','➣','➤','✦','✧','△','❀','○','□','♤','♡','◇','♧','々','〆']
+global.tempatDB = 'database.json' // Taruh url mongodb di sini jika menggunakan mongodb. Format : 'mongodb+srv://...'
+global.tempatStore = 'baileys_store.json' // Taruh url mongodb di sini jika menggunakan mongodb. Format : 'mongodb+srv://...'
+global.pairing_code = true
+global.number_bot = '' // Kalo pake panel bisa masukin nomer di sini, jika belum ambil session. Format : '628xx'
+
+global.fake = {
+	anonim: 'https://telegra.ph/file/95670d63378f7f4210f03.png',
+	thumbnailUrl: 'https://telegra.ph/file/fe4843a1261fc414542c4.jpg',
+	thumbnail: fs.readFileSync('./src/media/naze.png'),
+	docs: fs.readFileSync('./src/media/fake.pdf'),
+	listfakedocs: ['application/vnd.openxmlformats-officedocument.spreadsheetml.sheet','application/vnd.openxmlformats-officedocument.presentationml.presentation','application/vnd.openxmlformats-officedocument.wordprocessingml.document','application/pdf'],
+}
+
+global.my = {
+	yt: '',
+	gh: '',
+	gc: '',
+	ch: '',
+}
+
+global.limit = {
+	free: 0,
+	premium: 1,
+	vip: 1
+}
+
+global.money = {
+	free: 0,
+	premium: 1,
+	vip: 1
+}
+
+global.mess = {
+	key: "Apikey limit! Silahkan Upgrade: https://naze.biz.id",
+	owner: "Khusus Owner!",
+	admin: "Khusus Admin!",
+	botAdmin: "Bot harus Admin!",
+	onWa: "Nomor tersebut tidak terdaftar di WhatsApp!",
+	group: "Khusus Grup!",
+	private: "Khusus Private Chat!",
+	quoted: "Reply pesannya!",
+	limit: "Limit habis!",
+	prem: "Khusus Premium!",
+	text: "Masukkan teksnya!",
+	media: "Kirim medianya!",
+	wait: "Proses...",
+	fail: "Gagal!",
+	error: "Error!",
+	done: "Selesai!"
+}
+
+global.APIs = {
+	naze: 'https://api.naze.biz.id',
+	neosantara: 'https://api.neosantara.xyz/v1',
+}
+global.APIKeys = {
+	'https://api.naze.biz.id': 'your_apikey_here',
+	'https://api.neosantara.xyz/v1': 'API_KEY_NEOSANTARA_AI',
+}
+
+// Lainnya
+global.jadwalSholat = {
+	Subuh: '04:30',
+	Dzuhur: '12:06',
+	Ashar: '15:21',
+	Maghrib: '18:08',
+	Isya: '19:00'
+}
+
+global.badWords = ['dongo', 'konsol'] // input kata-kata toxic yg lain. ex: ['dongo','dongonya']
+global.chatLength = 1000
+
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+	fs.unwatchFile(file)
+	console.log(chalk.yellowBright(`[UPDATE] ${__filename}`))
+	delete require.cache[file]
+	require(file)
+});
